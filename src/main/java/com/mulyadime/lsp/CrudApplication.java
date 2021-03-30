730febad7a7158a4805ccc9e515113d612f36d12
Hamid Mulyadi <hamid.mulyadi@anabatic.com>
2021-03-30 08:52:56 +0700

    Initial commit

---
+++ src/main/java/com/mulyadime/lsp/CrudApplication.java
@@ -0,0 +1,13 @@
+package com.mulyadime.lsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApplication.class, args);
+	}
+
+}
